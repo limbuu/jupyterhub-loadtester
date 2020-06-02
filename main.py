c79dd24700f6304a45b7dc9fbab58fcf2e7b9d42
limbuu <iammanshi116@gmail.com>
2020-06-02 14:51:31 +0545

    added argparse for parsing commandline arguments

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import logging
 import logging.config
 logging.config.fileConfig(fname='logger.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
+import argparse
 
 USERNAME_PREFIX = 'user'
 
@@ -19,12 +20,16 @@ def main(hub_url, user_count):
         events.append(simulator.simulate_user(hub_url=hub_url,username=USERNAME_PREFIX+'-'+str(i)))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.gather(*events))
-    
+
 if __name__=='__main__':
     # hub_url = 'http://34.69.94.109:8000'
-    hub_url = 'http://localhost:9000'
-    user_count = 2
+    # hub_url = 'http://localhost:9000'
+    # user_count = 2
     logger.info('*************** Started the Jupyterhub Loadtesting ****************')
-    main(hub_url, user_count)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hub_url",  help="this is hub url used for load testing purpose", type=str)
+    parser.add_argument("user_count",  help="this is number of users for simulating in load testing environment", type=int)
+    args = parser.parse_args()
+    main(args.hub_url, args.user_count)
     logger.info('*************** Stopping the Jupyterhub Loadtesting ****************')
 
